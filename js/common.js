4c77677ff409b8ac24e88169a2cf5060d6216a19
Vi Ngoc Tien <twhite3000@yandex.ru>
2015-04-06 23:16:44 +0300

    2nd tutorial done

--- js/common.js
+++ js/common.js
@@ -1,5 +1,23 @@
 $(document).ready(function() {
 
-	
-	
+	function heightDetect() {
+		$(".main_head").css("height", $(window).height());
+	};
+	heightDetect();
+	$(window).resize(function(){
+		heightDetect();
+	});
+
+	$(".toggle_mnu, .menu_item").click(function() {
+		$(".sandwich").toggleClass("active");
+	});
+
+	$('.toggle_mnu').on('click', function(){
+		$('.top_mnu').fadeToggle(600);
+		$(".top_mnu li a").toggleClass("fadeInUp animated")
+	})
+});
+$(window).load(function() { 
+	$(".loader_inner").fadeOut(); 
+	$(".loader").delay(400).fadeOut("slow"); 
 });
