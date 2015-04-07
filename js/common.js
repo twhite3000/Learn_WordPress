1f33fcdcb50533034f9bbb33bbcb00abaa671506
Vi Ngoc Tien <twhite3000@yandex.ru>
2015-04-07 22:57:00 +0300

    6 tutorial done

--- js/common.js
+++ js/common.js
@@ -1,8 +1,14 @@
 $(document).ready(function() {
+	$(document).ready(function() {
+		$(".popup").magnificPopup({type:"image"});
+	});
 
 	$(".top_text h1").animated("fadeInDown", "fadeOutUp");
 	$(".top_text p").animated("fadeInUp", "fadeOutDown");
 	$(".top_text p, .section_header").animated("fadeInUp", "fadeOutDown");
+	$(".animation_2").animated("fadeInLeft", "fadeOutLeft");
+	$(".animation_3").animated("fadeInRight", "fadeOutRight");
+	$(".animation_1").animated("flipInY", "flipOutY");
 
 	function heightDetect() {
 		$(".main_head").css("height", $(window).height());
