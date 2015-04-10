c717423eb859982a50dc4af9320d23caf7c00ec1
Vi Ngoc Tien <twhite3000@yandex.ru>
2015-04-10 23:40:13 +0300

    8 tutorial done
    Too close to win!

--- js/common.js
+++ js/common.js
@@ -1,7 +1,23 @@
 $(document).ready(function() {
+
+	$(".portfolio_item").each(function(i) {
+		$(this).find("a").attr("href", "#work_" + i);
+		$(this).find(".port_descr").attr("id", "work_" + i);
+	});
+
+	$("#portfolio_grid").mixItUp();
+
+	$(".s_portfolio li").click(function() {
+		$(".s_portfolio li").removeClass("active");
+		$(this).addClass("active");
+	});
+
 	$(document).ready(function() {
 		$(".popup").magnificPopup({type:"image"});
 	});
+
+	$(".popup").magnificPopup({type: "image"});
+	$(".popup_content").magnificPopup({type: "inline", midClick: true})
 
 	$(".top_text h1").animated("fadeInDown", "fadeOutUp");
 	$(".top_text p").animated("fadeInUp", "fadeOutDown");
@@ -30,6 +46,7 @@ $(document).ready(function() {
 		$(".top_text").toggleClass("h_opacify")
 		$(".toggle_mnu").toggleClass("container toggle_mnu_active")
 	});
+	$("input, select, textarea").not("[type=submit]").jqBootstrapValidation();
 });
 $(window).load(function() { 
 	$(".loader_inner").fadeOut(); 
